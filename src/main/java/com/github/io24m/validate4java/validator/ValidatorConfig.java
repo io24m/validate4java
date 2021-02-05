340c96d00c551f5a98de7b9c9211391132504228
io24m <io24m@outlook.com>
2021-02-05 17:01:22 +0800

    i

--- src/main/java/com/github/io24m/validate4java/validator/ValidatorConfig.java
+++ src/main/java/com/github/io24m/validate4java/validator/ValidatorConfig.java
@@ -6,7 +6,31 @@ package com.github.io24m.validate4java.validator;
  * @create 2021-02-05 13:32
  */
 public class ValidatorConfig {
-    public boolean check;
-    public boolean pass;
-    public String errorMessage;
+    private boolean check;
+    private boolean pass;
+    private String errorMessage;
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
+
+    public boolean isPass() {
+        return pass;
+    }
+
+    public void setPass(boolean pass) {
+        this.pass = pass;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
 }
